76a720be3d69b9d2d98290110158abe23ba0235a
zacharing <zaw9998@saintjoe.edu>
2015-02-23 16:33:43 -0500

    To keep track of Phones#

--- Phone.java
+++ Phone.java
@@ -4,20 +4,20 @@ public class Phone {
 	public int quantity;
 	
 //Above are the data members
-
+//How the first one when printed will appear
 Phone() {
 	make = "Unknown";
 	model = "Android";
 	quantity = 1;
 	}
-
+//Doing this in order to make them seen
 Phone(String thisMake, String thisModel, int thisQuantity) {
 	this();
 	make = thisMake;
 	model = thisModel;
 	quantity = thisQuantity;	
 	}
-
+//How it will look when it is printed
 public String toString() {
 	String output = "";
 	output = "Make: "  +  make  +  "Model: "  +  model  +  "Quantity: "  +  quantity;
